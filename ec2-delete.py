b2713ce465bcdd650672348478bc0809de897633
pporumbescu <60489736+pporumbescu@users.noreply.github.com>
2024-03-16 17:20:02 +0000

    improved delete ec2 script

--- ec2-delete.py
+++ ec2-delete.py
@@ -1,17 +1,22 @@
 import boto3
+from botocore.exceptions import ClientError
 
 # Specify the instance IDs and their corresponding regions
 instances = {
     'i-09ce071be381753ca': 'eu-central-1',
-    'i-0d150bb05088a7df6': 'eu-west-3'
+    'i-0dc792a201b483aff': 'us-east-1'
 }
 
 for instance_id, region in instances.items():
     ec2_client = boto3.client('ec2', region_name=region)
 
-    # Terminate the specified EC2 instance in the current region
-    response = ec2_client.terminate_instances(InstanceIds=[instance_id])
+    try:
+        # Attempt to terminate the specified EC2 instance in the current region
+        response = ec2_client.terminate_instances(InstanceIds=[instance_id])
 
-    # Print the termination status of the instance
-    for instance in response['TerminatingInstances']:
-        print(f"Instance {instance['InstanceId']} in {region} - Current State: {instance['CurrentState']['Name']}")
+        # Print the termination status of the instance
+        for instance in response['TerminatingInstances']:
+            print(f"Instance {instance['InstanceId']} in {region} - Current State: {instance['CurrentState']['Name']}")
+
+    except ClientError as error:
+        print(f"Failed to terminate instance {instance_id} in {region}. Error: {error}")

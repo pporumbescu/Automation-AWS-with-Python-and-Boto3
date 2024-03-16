8fa284a7b7cb2909a27665971e2ff16a06269dae
pporumbescu <60489736+pporumbescu@users.noreply.github.com>
2024-03-16 16:52:01 +0000

    Improved file Create ec2

--- ec2-create.py
+++ ec2-create.py
@@ -1,33 +1,50 @@
 import boto3
-import schedule
+from botocore.exceptions import ClientError
+import os
 
+# Initialize the EC2 client and resource from Boto3
 ec2_client = boto3.client('ec2')
 ec2 = boto3.resource('ec2')
 
-
-instances_create = ec2.create_instances(
-    ImageId='ami-0f403e3180720dd7e',
-    MinCount=1,
-    MaxCount=1,
-    InstanceType='t2.micro',
-    TagSpecifications=[
-        {
-            'ResourceType': 'instance',
-            'Tags': [
+def create_ec2_instance(image_id, instance_type, instance_name):
+    """
+    Create an EC2 instance with specified image ID, instance type, and name.
+    """
+    try:
+        instances = ec2.create_instances(
+            ImageId=image_id,
+            MinCount=1,
+            MaxCount=1,
+            InstanceType=instance_type,
+            TagSpecifications=[
                 {
-                    'Key': 'Name',
-                    'Value': 'new-ec2'
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': instance_name
+                        }
+                    ]
                 }
             ]
-        }
-    ]
-)
+        )
+        instance = instances[0]
+        print(f"Creating instance {instance.id}...")
+        instance.wait_until_running()
+        print(f"Instance {instance.id} is now running.")
+        return instance
+    except ClientError as e:
+        print(f"An error occurred: {e}")
+        return None
 
-# Wait for the instance to be running before describing it
-instances_create[0].wait_until_running()
+if __name__ == '__main__':
+    # Use environment variables or replace with your own values
+    image_id = os.getenv('AWS_EC2_AMI_ID', 'ami-0f403e3180720dd7e')
+    instance_type = os.getenv('AWS_EC2_INSTANCE_TYPE', 't2.micro')
+    instance_name = 'new-ec2'  # Consider generating or passing this dynamically for multiple instances
 
-# Refresh the instance object to get the latest state
-instances_create[0].load()
-
-print(instances_create[0].id)
+    # Create the EC2 instance
+    instance = create_ec2_instance(image_id, instance_type, instance_name)
+    if instance:
+        print(f"Successfully created and started instance {instance.id} with name '{instance_name}'.")
 
